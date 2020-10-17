d52ab4ff3bfe9cdb65aa41ff8499438e57f70564
Marcel Kallen <mk@fluxter.net>
2020-10-17 23:05:19 +0200

    changed build

--- build.sh
+++ build.sh
@@ -1,11 +1,10 @@
-docker build ./prod --build-arg DOCKER_TAG=7.4 -t fluxter/web-php:7.4
-docker build ./prod --build-arg DOCKER_TAG=7.3 -t fluxter/web-php:7.3
+function buildTag
+{
+    docker build ./prod --build-arg DOCKER_TAG=$1 -t fluxter/web-php:$1
+    docker build ./dev --build-arg DOCKER_TAG=$1 -t fluxter/web-php:$1-dev
+    docker push fluxter/web-php:$1
+    docker push fluxter/web-php:$1-dev
+}
 
-docker build ./dev --build-arg DOCKER_TAG=7.4 -t fluxter/web-php:7.4-dev
-docker build ./dev --build-arg DOCKER_TAG=7.3 -t fluxter/web-php:7.3-dev
-
-docker push fluxter/web-php:7.3
-docker push fluxter/web-php:7.4
-
-docker push fluxter/web-php:7.3-dev
-docker push fluxter/web-php:7.4-dev
+buildTag 7.3
+buildTag 7.4
