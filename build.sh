7b6e28adad24fa5d8ccb21b5a5bd919f54ad72a8
Marcel Kallen <mk@fluxter.net>
2020-10-19 15:55:01 +0200

    added cron to container and python to dev

--- build.sh
+++ build.sh
@@ -1,9 +1,24 @@
 function buildTag
 {
     docker build ./prod --build-arg DOCKER_TAG=$1 -t fluxter/web-php:$1
+    if [ $? != 0 ]; then
+        echo Build resulted in an error
+    fi
+
     docker build ./dev --build-arg DOCKER_TAG=$1 -t fluxter/web-php:$1-dev
+    if [ $? != 0 ]; then
+        echo Build resulted in an error
+    fi
+
     docker push fluxter/web-php:$1
+    if [ $? != 0 ]; then
+        echo Push resulted in an error
+    fi
+
     docker push fluxter/web-php:$1-dev
+    if [ $? != 0 ]; then
+        echo Push resulted in an error
+    fi
 }
 
 buildTag 7.3
