6257a6d0fe47bc8f74e1835561c502f436cca3c2
Marcel Kallen <mk@tecno.de>
2020-10-29 16:01:30 +0100

    fixed perfomance in dev container

--- build.sh
+++ build.sh
@@ -10,14 +10,18 @@ function buildTag
         echo Build resulted in an error
     fi
 
-    docker push fluxter/web-php:$1
-    if [ $? != 0 ]; then
-        echo Push resulted in an error
-    fi
+    if [ "$1" == "push" ]; then
+        docker push fluxter/web-php:$1
+        if [ $? != 0 ]; then
+            echo Push resulted in an error
+        fi
 
-    docker push fluxter/web-php:$1-dev
-    if [ $? != 0 ]; then
-        echo Push resulted in an error
+        docker push fluxter/web-php:$1-dev
+        if [ $? != 0 ]; then
+            echo Push resulted in an error
+        fi
+    else
+        echo "Not pushing, run command with push like this: $0 push"
     fi
 }
 
