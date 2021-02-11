a5f84aacf00d483bd70b1821e4e41c3cb0c060de
Marcel Kallen <mk@fluxter.net>
2021-02-11 17:08:41 +0100

    added /tmp/CONTAINER_STARTED and changed supervisord config to allow custom configs

--- prod/rootfs/scripts/entrypoint.sh
+++ prod/rootfs/scripts/entrypoint.sh
@@ -5,4 +5,4 @@ echo "Starting Symfony Web Container with NGINX and PHP 7.4"
 source /scripts/settings.sh
 run-parts /scripts/startup &
 
-/usr/bin/supervisord -n -c /etc/supervisord.conf 
+/usr/bin/supervisord
