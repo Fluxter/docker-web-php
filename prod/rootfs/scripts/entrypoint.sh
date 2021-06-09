fcd42be0f7a3f27e4080ccd5c810c42ecc1a9830
Marcel Kallen <github@mkpost.de>
2021-06-09 23:42:36 +0200

    Updated repo

--- prod/rootfs/scripts/entrypoint.sh
+++ prod/rootfs/scripts/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "Starting Symfony Web Container with NGINX and PHP 7.4"
+echo "[STARTUP] Starting Symfony Web Container with NGINX and PHP"
 
 source /scripts/settings.sh
-run-parts /scripts/startup -v &
+run-parts /scripts/startup &
 
 /usr/bin/supervisord
--- prod/rootfs/scripts/settings.sh
+++ prod/rootfs/scripts/settings.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-export FILE_PARAMETERS_DIST=$APP_ROOT/.env.local.dist
-export FILE_PARAMETERS=$APP_ROOT/.env.local
-
 export DOCKER_HOST_IP=`ip -4 route list match 0/0 | awk '{print $3}'`
