316579b822ef0cffd768a41f627e4773f69a7b22
Marcel Kallen <github@mkpost.de>
2021-06-12 12:09:07 +0200

    renamed docker_tag to php_version

--- build.sh
+++ build.sh
@@ -18,8 +18,8 @@ fi
 
 echo "Builiding version $VERSION with tag prefix $TAG and mode $MODE"
 if [ "$MODE" = "build" ]; then 
-    docker build ./prod --build-arg DOCKER_TAG=$VERSION -t fluxter/web-php:$TAG --no-cache
-    docker build ./dev --build-arg DOCKER_TAG=$VERSION --build-arg BASE_IMG=fluxter/web-php:$TAG -t fluxter/web-php:$TAG-dev --no-cache
+    docker build ./prod --build-arg PHP_VERSION=$VERSION -t fluxter/web-php:$TAG --no-cache
+    docker build ./dev --build-arg PHP_VERSION=$VERSION --build-arg BASE_IMG=fluxter/web-php:$TAG -t fluxter/web-php:$TAG-dev --no-cache
 elif [ "$MODE" = "push" ]; then 
     docker push fluxter/web-php:$TAG
     docker push fluxter/web-php:$TAG-dev
