4c89c40d924113d20910842893da51edcb439eac
Marcel Kallen <github@mkpost.de>
2021-08-28 10:44:42 +0200

    SSL Keys are now generated as www-data

--- prod/rootfs/scripts/entrypoint-nginx.sh
+++ prod/rootfs/scripts/entrypoint-nginx.sh
@@ -10,7 +10,7 @@ FILE_SSL_KEY=/home/www-data/ssl/nginx-app.key
 FILE_SSL_CRT=/home/www-data/ssl/nginx-app.crt
 if [ ! -f "$FILE_SSL_KEY" ]; then
     echo "Generating new self-signed keys!"
-    openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $FILE_SSL_KEY -out $FILE_SSL_CRT -subj "/C=US/ST=some-state/L=some-city/O=some-organisation/CN=symfony-app"
+    sudo -u www-data openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout $FILE_SSL_KEY -out $FILE_SSL_CRT -subj "/C=US/ST=some-state/L=some-city/O=some-organisation/CN=symfony-app"
 fi
 
 nginx
